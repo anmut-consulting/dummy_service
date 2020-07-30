aebcd9ae0057cfbc926ec66535130ba9f7ad3790
jorigeysen <jori.geysen@anmut.co.uk>
2020-07-30 22:01:32 +0200

    first commit with article db

--- dummy_service/app/models.py
+++ dummy_service/app/models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from app import db
 
 
@@ -11,11 +9,11 @@ from app import db
 # class, which takes the field type as an argument, plus other optional arguments that, for example,
 # allow me to indicate which fields are unique and indexed, which is important so that database
 # searches are efficient.
-class User(db.Model):
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    title = db.Column(db.String(64), index=True)
+    article_body = db.Column(db.String(120), index=True)
+    # date = db.Column(db.DateTime, index=True, unique=False)
     # we add here a relationship field. this is not an actual database field, but a high level view of the
     # relationship between users and posts. For a one-to-many relationship, a db.relationship field is normally
     # defined on the "one" side, and is used as a convenient way to get access to the 'many'. The first argument
@@ -24,25 +22,9 @@ class User(db.Model):
     # the name of a field that will be added to the objects of the "many" class that points back at the "one" object.
     # This will add a post.author expression that will return the user given a post. The lazy argument defines how the
     # database query for the relationship will be issued.
-    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    # posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     # The __repr__ method tells Python how to print objects of this class, which is going to be
     # useful for debugging.
     def __repr__(self):
-        return "<User {}>".format(self.username)
-
-
-# The new post class will represent blog posts written by users. The timestamp field is indexed, which is
-# useful if you want to retrieve posts in chronological order. I have also added a default argument, and passed
-# the datetime.utcnow function. When you pass a function as a default, SQLAlchemy will set the field to the value
-# of calling that function (note that I did not include the () after utcnow, so I'm passing the function itself,
-# not the result of calling it.). In general you will want to work with UTC dates and times in a server application.
-#
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
-    def __repr__(self):
-        return "<Post {}>".format(self.body)
+        return "<Article {}>".format(self.title)
--- dummy_service/app/routes.py
+++ dummy_service/app/routes.py
@@ -1,7 +1,18 @@
-from app import app
+from app import app, db
+from app.models import Article
+from flask import request
 
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return "Hello, World!"
+# first api call: /add_article
+# this call should add a single article and should be accompanied with a dictionary containing
+# title and article_body.
+@app.route("/add_article", methods=["POST"])
+def add_article():
+    article = Article(
+        title=request.json["title"],
+        article_body=request.json["article_body"],
+        # date = request.json['date'],
+    )
+    db.session.add(article)
+    db.session.commit()
+    return {"message": "success"}
